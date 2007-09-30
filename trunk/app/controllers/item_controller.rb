9e9c6a8ee27d9b3fbebc876a95ada2924ea93035
eckhart.koeppen <eckhart.koeppen@4c73f7fb-593b-0410-b116-b3c2c13edfc6>
2007-09-30 17:33:15 +0000

    Nesting changes

--- trunk/app/controllers/item_controller.rb
+++ trunk/app/controllers/item_controller.rb
@@ -1,8 +1,8 @@
 class ItemController < ApplicationController
     active_scaffold :item do |config|
         config.label = "Product Backlog"
-#        config.columns = [:title, :description, :priority, :effort, :status, :notes]
-        config.nested.add_link("Tasks", [:task])
+        config.list.columns = [:title, :description, :priority, :effort, :status, :notes]
+        config.nested.add_link("Tasks", [:tasks])
     end
     layout "activescaffold"
 end

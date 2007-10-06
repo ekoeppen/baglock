5f7d9215882aab90133d07d1dc9166ccd84f3115
eckhart.koeppen <eckhart.koeppen@4c73f7fb-593b-0410-b116-b3c2c13edfc6>
2007-10-06 15:54:17 +0000

    Burndown changes

--- trunk/app/controllers/sprint_controller.rb
+++ trunk/app/controllers/sprint_controller.rb
@@ -2,11 +2,14 @@ class SprintController < ApplicationController
   active_scaffold :sprint do |config|
     config.list.sorting = { :title => :asc }
     config.nested.add_link("Items", [:items])
+    
+    config.columns =
+      [:title, :start, :end, :total_points, :total_effort, :effort_per_day]
 
     config.list.columns =
       [:title, :start, :end, :total_points, :total_effort]
     config.show.columns = 
-      [:title, :start, :end, :total_points, :total_effort]
+      [:title, :start, :end, :total_points, :total_effort, :effort_per_day]
     config.create.columns = 
       [:title, :start, :end]
     config.update.columns =
@@ -14,6 +17,8 @@ class SprintController < ApplicationController
     config.columns[:items].form_ui = :select
     config.columns[:start].form_ui = :calendar
     config.columns[:end].form_ui = :calendar
+    
+    config.columns[:effort_per_day].form_ui = :graph
   end
   
   layout "activescaffold"

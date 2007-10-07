ca53bb037380a9bd10572fe168aa6e4e55e4ac75
eckhart.koeppen <eckhart.koeppen@4c73f7fb-593b-0410-b116-b3c2c13edfc6>
2007-10-07 18:43:56 +0000

    Change available columns

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,6 +1,7 @@
 class TaskController < ApplicationController
   active_scaffold :task do |config|
     config.columns[:item].form_ui = :select
+    config.columns[:developer].form_ui = :select
     config.columns[:description].form_ui = :textarea
     config.columns[:description].options = { :rows => 25, :cols => 80 }
     config.columns[:id].label = "ID"

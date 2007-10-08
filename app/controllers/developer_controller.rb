be7a5d418a3480883b92e75aab4dfa1bb6c311a7
eckhart.koeppen <eckhart.koeppen@4c73f7fb-593b-0410-b116-b3c2c13edfc6>
2007-10-08 12:48:01 +0000

    Changed developer_controller.rb

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -1,6 +1,15 @@
 class DeveloperController < ApplicationController
   active_scaffold :developer do |config|
     config.columns[:tasks].form_ui = :select
+    config.list.columns =
+      [:name, :tasks]
+    config.show.columns = 
+      [:name, :tasks]
+    config.create.columns =
+      [:name]
+    config.update.columns =
+      [:name, :tasks]
+    config.nested.add_link("Tasks", [:tasks])
   end
 
   layout "activescaffold"
